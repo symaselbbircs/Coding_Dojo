af0720d64c586bd5a04607a170fbe8e04cd5200d
Ames <ameyers@flowroute.com>
2016-10-27 02:26:51 -0700

    registration assignment commit

---
+++ django/Registration/apps/registration/urls.py
@@ -0,0 +1,9 @@
+from django.conf.urls import url
+from . import views as v
+
+urlpatterns = [
+    url(r'^$', v.index, name="main"),
+    url(r'^register$', v.register, name="register"),
+    url(r'^login$', v.login, name="login"),
+    url(r'^success$', v.success, name="success")
+]
---
+++ django/Registration/apps/registration/views.py
@@ -0,0 +1,70 @@
+from django.shortcuts import render, redirect
+from django.core.urlresolvers import reverse
+from django.contrib import messages
+from django.http import HttpResponse
+from models import Users
+
+# Create your views here.
+def index(request):
+    return render(request,
+    'registration/index.html')
+
+def register(request):
+    if request.method == "POST":
+        first_name = request.POST['first_name']
+        last_name = request.POST['last_name']
+        email = request.POST['email']
+        password = request.POST['password1']
+        c_password = request.POST['password2']
+
+        if Users.objects.filter(email=email):
+            messages.error(request, "Ruh Roh! Email is already in use.")
+            return redirect(reverse('main'))
+
+        result = Users.objects.register(first_name = first_name, last_name =last_name, email=email, password1=password, password2 = c_password)
+        if result[0]:
+            print "No Pass! {}".format(result[1])
+            for row in result[1]:
+                messages.error(request, row)
+            return redirect(reverse('main'))
+        else:
+            hashed = Users.objects.create_password(password)
+            user = Users(first_name = first_name, last_name = last_name, email= email, password = hashed)
+            user.save()
+            try:
+                u = Users.objects.get(email=email)
+            except:
+                return HttpResponse("Ruh Roh! Something went wrong.v Please contact site administrator.")
+
+            request.session['first_name'] = u.first_name
+            messages.success(request,"Successfully registered! (or logged in)")
+            return redirect(reverse('success'))
+    else:
+        return redirect(reverse('index'))
+    pass
+
+def login(request):
+    if request.method == "POST":
+        email = request.POST['email']
+        password = request.POST['password']
+        try:
+            user = Users.objects.get(email = email)
+        except:
+            messages.error(request, "User not found!")
+            return redirect(reverse('main'))
+        if user and Users.objects.verify_password(password,user.password):
+            # request.session['first_name'] = user.first_name
+            # request.sesssion['last_name'] = user.last_name
+            # request.session['uid'] = user.id
+            request.session['first_name'] = user.first_name
+            messages.success(request,"Successfully registered! (or logged in)")
+            return redirect(reverse('success'))
+        else:
+            messages.error(request, "Incorrect Password!")
+            return redirect(reverse('main'))
+    else:
+        return redirect(reverse('main'))
+
+def success(request):
+    return render(request,
+    'registration/success.html')
