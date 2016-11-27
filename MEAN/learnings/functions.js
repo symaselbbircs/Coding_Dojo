d8c19fec8a0492eb28bd365b15f38f56afb9278b
Ames <ameyers@flowroute.com>
2016-11-26 21:09:39 -0800

    Remove learnings files

--- MEAN/learnings/functions.js
+++
@@ -1,8 +0,0 @@
-function myFunctionName(firstParam, secondParam){
-  var myProduct = firstParam * secondParam;
-  return myProduct
-}
-
-console.log(myFunctionName("hi", "hello"))
-
-console.log(myFunctionName(3,4))
