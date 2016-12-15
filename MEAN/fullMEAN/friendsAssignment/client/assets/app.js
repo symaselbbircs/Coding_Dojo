2933673a8fda5e66e79e9e2e18e47c3a3547c912
Ames <ameyers@flowroute.com>
2016-12-15 00:09:42 -0800

    Add controllers and factories

--- MEAN/fullMEAN/friendsAssignment/client/assets/app.js
+++ MEAN/fullMEAN/friendsAssignment/client/assets/app.js
@@ -3,9 +3,11 @@ var app = angular.module('app', ['ngRoute'])
 app.config(function ($routeProvider, $locationProvider, $provide) {
   $routeProvider
   .when('/', {
-    templateUrl: 'partials/new.html'
+    templateUrl: 'partials/new.html',
+    controller: 'newController'
   })
   .when('/edit',{
-    templateUrl: 'partials/edit.html'
+    templateUrl: 'partials/edit.html',
+    controller: 'editController'
   })
 });
