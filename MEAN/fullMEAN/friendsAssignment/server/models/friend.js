91aef0c01b778db442088e3a80c66e42e94e0389
Ames <ameyers@flowroute.com>
2016-12-15 20:37:44 -0800

    update friends model

--- MEAN/fullMEAN/friendsAssignment/server/models/friend.js
+++ MEAN/fullMEAN/friendsAssignment/server/models/friend.js
@@ -4,8 +4,9 @@ var mongoose = require('mongoose');
 
 var FriendsSchema = new Schema({
   first_name: {type:String, required:true},
-  last_name: {type:String, required: true}
-})
+  last_name: {type:String, required: true},
+  birthday: {type:Date, required: true}
+},{timestamp:true})
 
 
 var Friend = mongoose.model("Friend", FriendsSchema)
